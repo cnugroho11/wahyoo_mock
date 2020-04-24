59e1944f01c3525add25e4b252bd8d98dc55a36d
cnugroho11 <cnugroho211@gmail.com>
2020-04-24 12:35:13 +0700

    voucher page

---
+++ lib/pages/voucher_page.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+class VoucherPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        bottomOpacity: 0,
+        elevation: 1.0,
+        backgroundColor: Colors.white,
+        leading: BackButton(
+          color: Colors.black54,
+        ),
+        title: Text(
+          'Voucher Anda',
+          style: TextStyle(fontSize: 15, color: Colors.black),
+        ),
+        actions: <Widget>[
+          IconButton(
+            onPressed: () {},
+            icon: Icon(
+              Icons.shopping_cart,
+              color: Colors.black54,
+            ),
+          ),
+          IconButton(
+            onPressed: () {},
+            icon: Icon(
+              Icons.contact_phone,
+              color: Colors.black54,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
--- lib/widgets/voucher_card.dart
+++ lib/widgets/voucher_card.dart
@@ -1,27 +1,53 @@
 import 'package:flutter/material.dart';
+import 'package:WahyooMock/pages/voucher_page.dart';
 
 class VoucherCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return  GestureDetector(
-      onTap: (){},
-      child: Container(
-        padding: EdgeInsets.only(bottom: 5),
-        height: 50,
-        width: 350,
-        decoration: BoxDecoration(
-          border: Border.all(color: Colors.green),
-          color: Colors.greenAccent,
-        ),
-        child: Center(
+    return  Container(
+      height: 50,
+      width: 350,
+      decoration: BoxDecoration(
+        border: Border.all(color: Colors.green),
+        color: Colors.greenAccent,
+      ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: <Widget>[
+          Container(
+            width: 210,
             child: Text(
-                'Anda Punya 2 Voucher',
+              'Anda Punya 2 Voucher',
               style: TextStyle(
-                fontSize: 14,
-                color: Colors.green
+                  fontSize: 14,
+                  color: Colors.green
               ),
-            )
-        ),
+            ),
+          ),
+          GestureDetector(
+            onTap: ()=> Navigator.of(context).push(MaterialPageRoute(
+                builder: (context) => VoucherPage()
+            )),
+            child: Container(
+                height: 30,
+                width: 60,
+                decoration: BoxDecoration(
+                    color: Colors.green,
+                    borderRadius: BorderRadius.circular(20)
+                ),
+                child: Center(
+                  child: Text(
+                    'LIHAT',
+                    style: TextStyle(
+                        fontSize: 11,
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold
+                    ),
+                  ),
+                )
+            ),
+          ),
+        ],
       ),
     );
   }
